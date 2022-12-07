ea664d2c538ded49df0f0f1fe55bc1f07c2896ec
Александр Чернобай <alex.chernobay@gmail.com>
2022-12-07 19:05:43 +0300

    errors removed, working version

--- Project/src/main/java/org/example/controller/BookRepository.java
+++ Project/src/main/java/org/example/controller/BookRepository.java
@@ -1,9 +1,11 @@
 package org.example.controller;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BookRepository extends CrudRepository<Book, Long> {
     List<Book> findByTitle(String title);
 }
